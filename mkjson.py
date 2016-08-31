c3b0d30e0a58ec183677bfb3ac27ddf73fc8aa34
morioking <morioking@gmail.com>
2016-09-01 00:11:16 +0900

    基本は完成
    ただしすでにあるノードを追加するときに、新しいノードとして追加されてしまう不具合あり

--- mkjson.py
+++ mkjson.py
@@ -7,28 +7,23 @@ import re
 import copy
 import random
 
-jsondata = []
-jsonout = []
-nodeclasses = []
-edgeclasses = []
-
 class JsonClass:
 	def __init__(self):
 		self.__file = ""
 	
 	def load_json(self, file):
 		f = open(file, "r")
-		self.__jsondata = json.load(f)
+		self.__data = json.load(f)
 		f.close()
 
 	def load_m3u8(self, file):
 		print "loading",file, "..."
 		f = open(file, "r")
 		labels = []
-		self.__jsondata = {"nodes":[], "edges":[]}
+		self.__data = {"nodes":[], "edges":[]}
 		for line in f:
 			if re.match("#EXTINF",line):
-				label = re.sub("#EXTINF(.*[,])","",line).strip()
+				label = re.sub("#EXTINF(.[0-9]{3,4}[,])","",line).strip()
 				labels.append(label)
 				print "    import ", label
 		f.close()
@@ -40,42 +35,42 @@ class JsonClass:
 		print "finish loading!"
 	
 	def show(self):
-		print json.dumps(self.__jsondata, indent = 4)
+		print json.dumps(self.__data, indent = 4)
 		
-	def get_json(self):
-		return self.__jsondata
+	def get_data(self):
+		return self.__data
 	
 	def get_nodes(self):
-		return self.__jsondata["nodes"]
+		return self.__data["nodes"]
 
 	def get_nodes_count(self):
-		return len(self.__jsondata["nodes"])
+		return len(self.__data["nodes"])
 		
 	def get_node_id(self, i):
-		return self.__jsondata["nodes"][i]["id"]
+		return self.__data["nodes"][i]["id"]
 
 	def get_node_size(self, i):
-		return self.__jsondata["nodes"][i]["size"]
+		return self.__data["nodes"][i]["size"]
 	
 	def get_node_size_with_id(self, id):
 		return self.get_node_size(self.get_node_index_with_id(id))
 		
 	def get_node_label(self, i):
-		return self.__jsondata["nodes"][i]["label"]
+		return self.__data["nodes"][i]["label"]
 	
 	def get_node_id_with_label(self, label):
 		id = "none"
-		for i in range(len(self.__jsondata["nodes"])):
-			if label.find(self.__jsondata["nodes"][i]["label"]) == 0:
+		for i in range(len(self.__data["nodes"])):
+			if label.find(self.__data["nodes"][i]["label"]) == 0:
 				# hit
-				id = self.__jsondata["nodes"][i]["id"]
+				id = self.__data["nodes"][i]["id"]
 
 		return id
 	
 	def get_node_index_with_id(self, id):
 		index = -1
-		for i in range(len(self.__jsondata["nodes"])):
-			if self.__jsondata["nodes"][i]["id"] == id:
+		for i in range(len(self.__data["nodes"])):
+			if self.__data["nodes"][i]["id"] == id:
 				index = i
 		return index
 	
@@ -92,325 +87,131 @@ class JsonClass:
 		pass
 	
 	def set_node_size(self, i, size):
-		self.__jsondata["nodes"][i]["size"] = size
+		self.__data["nodes"][i]["size"] = size
 
 	def set_node_size_with_id(self, id, size):
 		self.set_node_size(self.get_node_index_with_id(id), size)
 
 	def set_node_id(self, i, id):
-		self.__jsondata["nodes"][i]["id"] = id
+		self.__data["nodes"][i]["id"] = id
 
 	def create_new_node(self, color, label, y, x, id, size):
-		self.__jsondata["nodes"].append({"color":color, "label":label, "y":y, "x":x, "id":id, "size":size})
+		self.__data["nodes"].append({"color":color, "label":label, "y":y, "x":x, "id":id, "size":size})
 		
 	def create_new_edge(self, color, source, id, target):
-		self.__jsondata["edges"].append({"color":color, "source":source, "id":id, "target":target})
+		self.__data["edges"].append({"color":color, "source":source, "id":id, "target":target})
 
 	def get_edge_id_with_source_target(self, source, target):
 		id = "none"
-		for i in range(len(self.__jsondata["edges"])):
-			if self.__jsondata["edges"][i]["source"] == source:
-				if self.__jsondata["edges"][i]["target"] == target:
+		for i in range(len(self.__data["edges"])):
+			if self.__data["edges"][i]["source"] == source:
+				if self.__data["edges"][i]["target"] == target:
 					# hit
-					id = self.__jsondata["edges"][i]["id"]
+					id = self.__data["edges"][i]["id"]
 		return id
 		
 	def get_edge_index_with_id(self, id):
 		index = -1
-		for i in range(len(self.__jsondata["edges"])):
-			if self.__jsondata["edges"][i]["id"] == id:
+		for i in range(len(self.__data["edges"])):
+			if self.__data["edges"][i]["id"] == id:
 				index = i
 		return index
 
 	def get_edges_count(self):
-		return len(self.__jsondata["edges"])
+		return len(self.__data["edges"])
 
 	def get_edge_source(self, i):
-		return self.__jsondata["edges"][i]["source"]
+		return self.__data["edges"][i]["source"]
 
 	def get_edge_source_with_id(self, id):
 		return self.get_edge_source(self.get_edge_index_with_id(id))
 	
 	def get_edge_target(self, i):
-		return self.__jsondata["edges"][i]["target"]
+		return self.__data["edges"][i]["target"]
 
 	def get_edge_target_with_id(self, id):
 		return self.get_edge_target(self.get_edge_index_with_id(id))
 		
 	def get_edge_id(self, i):
-		return self.__jsondata["edges"][i]["id"]
+		return self.__data["edges"][i]["id"]
 
 	def set_edge_id(self, i, id):
-		self.__jsondata["edges"][i]["id"] = id
+		self.__data["edges"][i]["id"] = id
 	
 	def set_edge_source(self, i, source):
-		self.__jsondata["edges"][i]["source"] = source
+		self.__data["edges"][i]["source"] = source
 	
 	def set_edge_source_with_id(self, id, source):
 		self.set_edge_source(self.get_edge_index_with_id(id), source)
 
 	def set_edge_target(self, i, target):
-		self.__jsondata["edges"][i]["target"] = target
+		self.__data["edges"][i]["target"] = target
 	
 	def set_edge_target_with_id(self, id, target):
 		self.set_edge_target(self.get_edge_index_with_id(id), target)
 		
+	def del_edge(self, i):
+		self.__data["edges"].pop(i)
+
+	def del_edge_with_id(self, id):
+		self.del_edge(self.get_edge_index_with_id(id))
 		
-class M3u8Class:
-	def __init__(self, file):
-		self.__file = file
-		self.__labels = []
-		f = open(file, "r")
-		for line in f:
-			print "import ",file, line.strip()
-			if re.match("#EXTINF",line):
-				self.__labels.append(re.sub("#EXTINF(.*[,])","",line).strip())
-		f.close()
-	
-	def get_label(self, i):
-		return self.__labels[i]
-		
-	def get_labels(self):
-		return self.__labels
+def mixplaylist(pl1, pl2):
+	list_pl1 = []
+	list_pl2 = []
+	list_outpl = []
+	f = open(pl1,"r")
+	i = 0
+	for line in f:
+		# first line EXT1M3U skipped
+		if i != 0:
+			list_pl1.append(line)
+		elif i == 0:
+			header = line
+		i += 1
+	f.close()
 
-	def show_labels(self):
-		for label in self.__labels:
-			print label
+	f = open(pl2,"r")
+	i = 0
+	for line in f:
+		# first line EXTM3U skipped
+		if i != 0:
+			list_pl2.append(line)
+		i += 1
+	f.close()
 
-class NodesClass:
-	def __init__(self):
-		self.__nodeclasses = []
+	# import pdb; pdb.set_trace()
+	idx_a = 0
+	idx_b = 0
+	while idx_a < len(list_pl1):
+		if idx_a != len(list_pl1):
+			list_outpl.append(list_pl1[idx_a])
+			idx_a += 1
+		if idx_a != len(list_pl1):
+			list_outpl.append(list_pl1[idx_a])
+			idx_a += 1
+		if idx_b != len(list_pl2):
+			list_outpl.append(list_pl2[idx_b])
+			idx_b += 1
+		if idx_b != len(list_pl2):
+			list_outpl.append(list_pl2[idx_b])
+			idx_b += 1
 
-	def append_node(self, nodeclass):
-		self.__nodeclasses.append(nodeclass)
-		
-	def get_nodes_count(self):
-		return len(self.__nodeclasses)
-	
-	def get_node(self, num):
-		return self.__nodeclasses[num]
+	list_outpl.insert(0, header)
 
+	tdatetime = dt.now()
+	tstr = tdatetime.strftime('%Y-%m-%d')
+	file3 = "MIXED HISTORY_"+tstr+".m3u8"
+	f = open(file3, "w")
+	for line in list_outpl:
+		f.write(line)
+	f.close()
 
-class NodeClass:
-	def __init__(self, label):
-		self._color = "rgb(0,255,0)"
-		self._label = label
-		self._y = 0
-		self._x = 0
-		self._id = ""
-		self._size = 1
-		self._isNew = False
+	print ""
+	print "finish mixing!!"
+	print "the mixed playlist file is",file3
+	print ""
 
-	def set_label(self, label):
-		self._label = label
-		
-	def get_label(self):
-		return self._label
-
-	def set_x(self, x):
-		self._x = x
-	
-	def get_x(self):
-		return self._x
-		
-	def set_y(self, y):
-		self._y = y
-
-	def get_y(self):
-		return self._y
-		
-	def set_color(self, color):
-		self._color = color
-
-	def get_color(self):
-		return self._color
-
-	def set_id(self, id):
-		self._id = id
-	
-	def get_id(self):
-		return self._id
-
-	def set_size(self, size):
-		self._size = size
-	
-	def get_size(self):
-		return self._size
-		
-	def set_is_new(self, isNew):
-		self._isNew = isNew
-
-	def get_is_new(self):
-		return self._isNew
-
-class EdgesClass:
-	def __init__(self):
-		self.__edgeclasses = []
-		
-	def get_edges_count(self):
-		return len(self.__edgeclasses)
-	
-	def get_edge(self, num):
-		return self.__edgeclasses[num]
-	
-class EdgeClass:
-	def __init__(self):
-		self._color = "rgb(128,128,128)"
-		self._source = ""
-		self._id = ""
-		self._target = ""
-		self._isNew = False
-		
-	def set_color(self,color):
-		self._color = color
-
-	def get_color(self):
-		return self._color
-		
-	def set_source(self, source):
-		self._source = source
-		
-	def get_source(self):
-		return self._source
-		
-	def set_id(self, id):
-		self._id = id
-	
-	def get_id(self):
-		return self._id
-		
-	def set_target(self, target):
-		self._target = target
-	
-	def get_target(self):
-		return self._target
-		
-	def set_is_new(self, isNew):
-		self._isNew = isNew
-	
-	def get_is_new(self):
-		return self._isNew
-
-
-def import_playlist(m3u8, json_class):
-	jsonout = copy.deepcopy(jsondata)
-	m3u8_class = M3u8Class(m3u8)
-	labels = m3u8_class.get_labels()
-	input_json_class = json_class
-
-	# making NodeClass and nodeclasses
-	new_node_id = len(input_json_class.get_nodes()) - 1
-	nodeclasses_idx = 0
-	for label in labels:
-		print label
-		if input_json_class.get_node_id_with_label(label) == "none":
-			print "    -> create new node"
-		else:
-			print "    -> update node"
-
-		# isHit = False
-		# idxHit = 0
-		# node_size = 1
-		# nodeclasses.append(NodeClass(label))
-		# for i in range(len(input_json_class.get_nodes())):
-			# if label.find(input_json_class.get_node_label(i)) == 0:
-				# # hit
-				# isHit = True
-				# idxHit = i
-				# break
-			# elif label.find(input_json_class.get_node_label(i)) == -1:
-				# # not-hit
-				# isHit = False
-			# else:
-				# print "unknown hit"
-
-		# nodeclasses[nodeclasses_idx].set_is_new(not(isHit))
-		# if isHit:
-			# id = input_json_class.get_node_id(idxHit)
-			# posx = random.uniform(-1,1)
-			# posy = random.uniform(-1,1)
-			# size = input_json_class.get_node_size(idxHit) + 1
-			# color = "rgb("+str(random.randint(0,255))+","+str(random.randint(0,255))+","+str(random.randint(0,255))+")"
-		# else:
-			# new_node_id += 1
-			# id = "n"+str(new_node_id)
-			# posx = random.uniform(-1,1)
-			# posy = random.uniform(-1,1)
-			# size = 1
-			# color = "rgb("+str(random.randint(0,255))+","+str(random.randint(0,255))+","+str(random.randint(0,255))+")"
-
-		# nodeclasses[nodeclasses_idx].set_id(id)
-		# nodeclasses[nodeclasses_idx].set_x(posx)
-		# nodeclasses[nodeclasses_idx].set_y(posy)
-		# nodeclasses[nodeclasses_idx].set_size(size)
-		# nodeclasses[nodeclasses_idx].set_color(color)
-		# nodeclasses_idx += 1
-
-	# making EdgeClass and edgeclasses
-	# for i in range(len(nodeclasses)-1):
-		# source = nodeclasses[i].get_id()
-		# target = nodeclasses[i+1].get_id()
-		# ec = EdgeClass()
-		# ec.set_source(source)
-		# ec.set_target(target)
-		# edgeclasses.append(ec)
-
-	# # search isNew for EdgeClass
-	# new_edge_id = len(input_json_class.get_json()["edges"]) - 1
-	# for ec in edgeclasses:
-		# isHit = False
-		# source = ec.get_source()
-		# target = ec.get_target()
-		# for i in range(len(input_json_class.get_json()["edges"])):
-			# if (input_json_class.get_json()["edges"][i]["source"] == source) and (input_json_class.get_json()["edges"][i]["target"] == target):
-				# isHit = True
-				# break
-			# else:
-				# isHit = False
-
-		# #print "edge",isHit
-		# ec.set_is_new(not(isHit))
-		# if isHit:
-			# id = input_json_class.get_json()["edges"][i]["id"]
-		# else:
-			# new_edge_id += 1
-			# id = "e"+str(new_edge_id)
-
-		# ec.set_id(id)
-
-	# # update jsonout for node
-	# for nc in nodeclasses:
-		# if nc.get_is_new():
-			# jsonout["nodes"].append({"color":nc.get_color(), "label":nc.get_label(), "y":nc.get_y(), "x":nc.get_x(), "id":nc.get_id(), "size":nc.get_size()})
-		# else:
-			# for i in range(len(input_json_class.get_nodes())):
-				# if input_json_class.get_nodes()[i]["id"] == nc.get_id():
-					# jsonout["nodes"][i]["color"] = nc.get_color()
-					# jsonout["nodes"][i]["x"] = nc.get_x()
-					# jsonout["nodes"][i]["y"] = nc.get_y()
-					# jsonout["nodes"][i]["size"] = nc.get_size()
-
-
-	# # update jsonout for edge
-	# for ec in edgeclasses:
-		# if ec.get_is_new():
-			# jsonout["edges"].append({"color":ec.get_color(), "source":ec.get_source(), "id":ec.get_id(), "target":ec.get_target()})
-
-	# print "------------------jsondata------------------"
-	# print json.dumps(jsondata, indent = 4)
-	# print "------------------jsonout------------------"
-	# print json.dumps(jsonout, indent = 4)
-	
-	# for nc in nodeclasses:
-	# 	print nc.get_label(), nc.get_is_new(), nc.get_x(), nc.get_y(), nc.get_color(), nc.get_id()
-
-	# for ec in edgeclasses:
-	# 	print ec.get_color(), ec.get_source(), ec.get_id(), ec.get_target(), ec.get_is_new()
-
-	# f = open ("data.json", "w")
-	# json.dump(jsonout, f)
-	# f.close()
-		
 if __name__ == "__main__":
 
 	# load json data
@@ -423,30 +224,33 @@ if __name__ == "__main__":
 		input_line = raw_input()
 		if input_line == "exit":
 			break
-		elif input_line == "import":
-			import_playlist("test.m3u8", data)
 		elif input_line == "mixpl":
 			print ""
-			print "mix 2 playlist mutually.....but this function is not implemented yet"
-			print "select fist playlist"
-			#pl1 = raw_input()
-			print "select second playlist"
-			#pl2 = raw_input()
-			print "output file"
-			#pl3 = raw_input()
+			print "mix 2 playlist mutually..."
+			print "input fist playlist"
+			pl1 = raw_input()
+			print "input second playlist"
+			pl2 = raw_input()
+			mixplaylist(pl1, pl2)
 		elif input_line == "load":
 			# sequence diagram
 			# https://creately.com/diagram/isi7szk61/ZdBFzRm5MscjkiipVY4ee11xM%3D
 			print "load m3u8 format playlist."
 			print "select m3u8 file..."
-			#input_file = raw_input()
-			input_file = "test.m3u8"
+			input_file = raw_input()
+			#input_file = "test.m3u8"
 			m3u8 = JsonClass()
 			m3u8.load_m3u8(input_file)
 
 			# update node id for m3u8 data
+			new_node_id_number = data.get_nodes_count()
 			for i in range(m3u8.get_nodes_count()):
-				m3u8.set_node_id(i, data.get_node_id_with_label(m3u8.get_node_label(i)))
+				id = data.get_node_id_with_label(m3u8.get_node_label(i))
+				if id == "none":
+					m3u8.set_node_id(i, "n"+str(new_node_id_number))
+					new_node_id_number += 1
+				else:
+					m3u8.set_node_id(i, id)
 			#m3u8.show()
 			# create edge for m3u8 data
 			for i in range(m3u8.get_nodes_count()-1):
@@ -459,21 +263,20 @@ if __name__ == "__main__":
 				m3u8.set_edge_id(i, data.get_edge_id_with_source_target(m3u8.get_edge_source(i), m3u8.get_edge_target(i)))
 			m3u8.show()
 		elif input_line == "set":
-			data.show()
 			print "set m3u8 to data"
 			# process nodes
 			new_node_id_number = data.get_nodes_count()
 			for i in range(m3u8.get_nodes_count()):
-				if m3u8.get_node_id(i) == "none":
+				m = re.search("\d+", m3u8.get_node_id(i))
+				#if m3u8.get_node_id(i) == "none":
+				if m.group() >= new_node_id_number:
 					posx = random.uniform(-1,1)
 					posy = random.uniform(-1,1)
 					size = 1
 					color = "rgb("+str(random.randint(0,255))+","+str(random.randint(0,255))+","+str(random.randint(0,255))+")"
-					data.create_new_node(color, m3u8.get_node_label(i), posy, posx, "n"+str(new_node_id_number), size)
-					new_node_id_number += 1
+					data.create_new_node(color, m3u8.get_node_label(i), posy, posx, m3u8.get_node_id(i), size)
 				else:
 					data.set_node_size_with_id(m3u8.get_node_id(i), data.get_node_size_with_id(m3u8.get_node_id(i)) + 1)
-			data.show()
 			# process edges
 			new_edge_id_number = data.get_edges_count()
 			for i in range(m3u8.get_edges_count()): 
@@ -482,6 +285,19 @@ if __name__ == "__main__":
 					id = "e"+str(new_edge_id_number)
 					data.create_new_edge(color, m3u8.get_edge_source(i), id, m3u8.get_edge_target(i))
 					new_edge_id_number += 1
+			data.show()
+		elif input_line == "commit":
+			f = open ("data.json", "w")
+			json.dump(data.get_data(), f)
+			f.close()
+			print "commit data.json"
+			break
+		elif input_line == "del":
+			print "type edges index"
+			id = raw_input()
+			data.del_edge_with_id(id)
+			data.show()
+		elif input_line == "show":
 			data.show()
 		else:
 			print "again"
